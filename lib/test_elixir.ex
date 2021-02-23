01c4bc037f7e2be179bc2fa593cdcda6a2305a3b
Wojtek Mach <wojtek@wojtekmach.pl>
2021-02-23 15:24:19 +0100

    Initial commit

---
+++ lib/test_elixir.ex
@@ -0,0 +1,18 @@
+defmodule TestElixir do
+  @moduledoc """
+  Documentation for `TestElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
